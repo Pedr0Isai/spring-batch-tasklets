91f5fedf5274fce0048c28c8a05c1ecded084da0
Pedro Isai <picuaquentzi@outlook.com>
2023-09-05 21:49:46 -0600

    Se agregan comments sobre el funcionamiento

--- src/main/java/com/batch/SpringBatchApplication.java
+++ src/main/java/com/batch/SpringBatchApplication.java
@@ -1,7 +1,6 @@
 package com.batch;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/com/batch/config/ApplicationConfig.java
+++ src/main/java/com/batch/config/ApplicationConfig.java
@@ -9,15 +9,15 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 public class ApplicationConfig {
 	
-	/**Configuración de la base de datos. En este caso es H2 pero se puede utilizar cualquiera*/
-	@Bean
-    DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:testdb");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("sa");
-        return dataSource;
-    }
+	/**Configuración de la base de datos. El ejecicio es con H2 por eso esta parte esta comentada, ya que Spring hará la conexión automaticamente*/
+//	@Bean
+//    DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.h2.Driver");
+//        dataSource.setUrl("jdbc:h2:mem:testdb");
+//        dataSource.setUsername("sa");
+//        dataSource.setPassword("sa");
+//        return dataSource;
+//    }
 	
 }
